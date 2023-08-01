c9560c620dcce40e87eff054a928f29801cc97be
misterelie <elie.kouadio@uvci.edu.ci>
2023-08-01 10:43:49 +0000

    modif index

--- resources/views/partials-front/all-rights.blade.php
+++ resources/views/partials-front/all-rights.blade.php
@@ -3,7 +3,7 @@
         <div class="row justify-content-end">
             <div class="col-lg-8 col-md-6">
                 <div class="d-flex align-items-center justify-content-center" style="height: 75px;">
-                    <p class="mb-0">&copy; <a class="text-white border-bottom" href="#"></a>Copyright © 2022 | Allô Service- Services & Prestations | Tous droits réservés.
+                    <p class="mb-0">&copy; <a class="text-white border-bottom" href="#"></a>Copyright © 2023 | Allô Service- Services & Prestations | Tous droits réservés.
                 </div>
             </div>
         </div>
